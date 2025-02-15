602c9118d8f7ef2a333bcaf052afd48fec54fe3d
IshmamF <ishmamf2003@gmail.com>
2025-02-15 00:25:32 -0500

    imported card component with glowing effect and centered div

---
+++ Interface/my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { GlowEffectCardBackground } from "./components/card";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center pt-10">
+      <GlowEffectCardBackground
+      text={'test'}
+      />
+    </div>
+  );
+}
